0e9731a744d5623fd9e2e3c10585f519027e8e3a
sheenxavi004 <sheenxavi004@gmail.com>
2021-01-10 00:38:30 +0530

    Student POST and DELETE, UPLOAD routes handled(Subjected to changed)

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -8,6 +8,60 @@ router.get('/', (req, res) => {
     res.send('Student details');
 });
 
+router.post('/', (req, res) => {
+    const passOutYear = req.body.passout;
+    const id_list = req.body.ids;
+    const courseCode = req.body.course_code;
+
+    const records = [];
+
+    for(let i = 0; i < id_list.length; i++) {
+        const row = [courseCode, id_list[i], passOutYear];
+        records.push(row);
+    }
+    console.log(records);
+
+    const ASSIGN_COURSE_QUERY = `INSERT IGNORE INTO course_assignment VALUES ?`;
+
+    const connection = await db.getConnection();
+    try {
+        await connection.query('START TRANSACTION');
+
+        await connection.query(ASSIGN_COURSE_QUERY, [records]);
+
+        await connection.query('COMMIT');
+        await connection.release();
+
+        res.send({ success: true, message: 'Course assigned successfully' });
+
+    } catch (err) {
+        await connection.query('ROLLBACK');
+        await connection.release();
+        
+        res.send({ success: false, message: err.message });
+    }
+
+});
+
+router.delete('/', async (req, res) => {
+
+    const passOutYear = req.body.passout;
+    const id_list = req.body.ids;
+    const courseCode = req.body.course_code;
+
+    console.log(records);
+    try {
+        for(let i = 0; i < id_list.length; i++) {
+            const UNASSIGN_STUDENT_QUERY = `DELETE FROM course WHERE course_code = "${courseCode}" and passout = "${passOutYear}" and student_id = "${id_list[i]}"`;
+            await db.query(UNASSIGN_STUDENT_QUERY);
+        }
+        res.send({ success: true, message: 'Students successfully unassigned!' });
+    } catch (err) {
+        res.send({ success: false, message: err.message });
+    }
+
+});
+
 router.post('/uploadStudentList', upload.single("file"), async (req, res) => {
 
     try {
@@ -18,9 +72,30 @@ router.post('/uploadStudentList', upload.single("file"), async (req, res) => {
         const students = await readXlsxFile(path);
         // skip header
         // students.shift();
-        return res.send(students);
+        const ADD_STUDENTS_QUERY = 'INSERT IGNORE INTO student VALUES ?';
+        
+        const records = [];
+        
+        for(let i = 0; i < students.length; i++) {
+            const row = [students[i].university, students[i].name, students[i].deptid, students[i].batch];
+                records.push(row);
 
-        // Save the rows to database
+        const connection = await db.getConnection();
+        try{
+            await connection.query('START TRANSACTION');
+            await connection.query(ADD_STUDENTS_QUERY, [records]);
+            await connection.query('COMMIT');
+            await connection.release();
+    
+            res.send({ success: true, message: 'Students added successfully' });
+    
+        } catch (err) {
+            await connection.query('ROLLBACK');
+            await connection.release();
+            
+            res.send({ success: false, message: err.message });
+        }
+    }
 
     } catch (error) {
         console.log(error);
