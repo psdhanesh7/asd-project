6243aed248c70325a32f8d67f9c7ebf39cdc99ea
sheenxavi004 <sheenxavi004@gmail.com>
2021-01-14 10:50:05 +0530

    Fixed destructuring

--- routes/calculation.js
+++ routes/calculation.js
@@ -41,47 +41,39 @@ function calculateCOPO(){
     
     let copomatrix = [];
     let record = [];
-    
 
 
     try {
-        copomatrix  = await db.query(FETCH_CO_PO_MAPPING_QUERY);
-        record = await db.query(FETCH_CO_VALUES_QUERY);
-    
-    } catch (err) {
-        return res.send({ success: false, message: err.message });
-    }
+       const [copomatrix]  = await db.query(FETCH_CO_PO_MAPPING_QUERY);
+       const [record] = await db.query(FETCH_CO_VALUES_QUERY);
+        // Converting query to array form
 
-    // Converting query to array form
+        const covalues = arrayer(record[0]);
 
-    const covalues = arrayer(record[0]);
+        const finalpos = []; //Contains final po values
 
-    const finalpos = []; //Contains final po values
-
-    let interSum = 0, divSum = 0, matrixlen = copomatrix.length;
-    for(let i = 0 ; i < matrixlen; i++)
-    {
-        if(i % 6 && i != matrixlen - 1)
+        let interSum = 0, divSum = 0, matrixlen = copomatrix.length;
+        for(let i = 0 ; i < matrixlen; i++)
         {
-            interSum += copomatrix[i].relation * covalues[i % 6];
-            divSum += copomatrix[i].relation;
-            continue;
+            if(i % 6 && i != matrixlen - 1)
+            {
+                interSum += copomatrix[i].relation * covalues[i % 6];
+                divSum += copomatrix[i].relation;
+                continue;
+            }
+            finalpos.push(interSum / divSum);
+            interSum = copomatrix[i].relation * covalues[i % 6];
+            divSum = copomatrix[i].relation; 
         }
-        finalpos.push(interSum / divSum);
-        interSum = copomatrix[i].relation * covalues[i % 6];
-        divSum = copomatrix[i].relation; 
-    }
-    
-    const records = [];
+        
+        const records = [];
 
-    for(let i = 0; i < finalpos.length; i++) {
-        const row = [i + 1, finalpos[i], courseCode, passoutYear];   // Records in the form po, value, course_code, year
-        records.push(row);
-    }
+        for(let i = 0; i < finalpos.length; i++) {
+            const row = [i + 1, finalpos[i], courseCode, passoutYear];   // Records in the form po, value, course_code, year
+            records.push(row);
+        }
 
-    const FEED_PO_VALUES_QUERY = 'INSERT IGNORE INTO po_attainment VALUES, ?';
-
-    try{
+        const FEED_PO_VALUES_QUERY = 'INSERT IGNORE INTO po_attainment VALUES, ?';
         await db.query(FEED_PO_VALUES_QUERY, [records]);
         return res.send({ success: true, message: 'PO values inserted added succesfully' });
 
@@ -160,57 +152,46 @@ function COConsolidation() {
     const FETCH_ASMT2_TOTAL_QUERY = `SELECT co1, co2, co3, co4, co5, co6 FROM assignment WHERE course_code = "${courseCode}" AND batch = ${passoutYear} AND assignment_no = 2`;
     const FETCH_CONSOLIDATED_ES_QUERY = `SELECT grade FROM end_sem_exam_marks WHERE course_code = "${courseCode}" AND batch = ${passoutYear}`;
 
-    let internalExam1 = [];
-    let internalExam2 = [];
-    let internalExam1Total = [];
-    let internalExam2Total = [];
-    let assignment1 = [];
-    let assignment2 = [];
-    let assignment1Total = [];
-    let assignment2Total = [];
-    let endSemGrades = [];
-
     try{
-        internalExam1 = await db.query(FETCH_CONSOLIDATED_IA1_QUERY);
-        internalExam2 = await db.query(FETCH_CONSOLIDATED_IA2_QUERY);
-        internalExam1Total = await db.query(FETCH_IA1_TOTAL_QUERY);
-        internalExam2Total = await db.query(FETCH_IA2_TOTAL_QUERY);
-        assignment1 = await db.query(FETCH_CONSOLIDATED_ASMT1_QUERY);
-        assignment2 = await db.query(FETCH_CONSOLIDATED_ASMT2_QUERY);
-        assignment1Total = await db.query(FETCH_ASMT1_TOTAL_QUERY);
-        assignment2Total = await db.query(FETCH_ASMT2_TOTAL_QUERY);
-        endSemGrades = await db.query(FETCH_CONSOLIDATED_ES_QUERY);
-
-    }catch(err){
-        return res.send({ success: false, message: err.message });
-    }
+      const [internalExam1] = await db.query(FETCH_CONSOLIDATED_IA1_QUERY);
+      const [internalExam2] = await db.query(FETCH_CONSOLIDATED_IA2_QUERY);
+      const [internalExam1Total] = await db.query(FETCH_IA1_TOTAL_QUERY);
+      const [internalExam2Total] = await db.query(FETCH_IA2_TOTAL_QUERY);
+      const [assignment1] = await db.query(FETCH_CONSOLIDATED_ASMT1_QUERY);
+      const [assignment2] = await db.query(FETCH_CONSOLIDATED_ASMT2_QUERY);
+      const [assignment1Total] = await db.query(FETCH_ASMT1_TOTAL_QUERY);
+      const [assignment2Total] = await db.query(FETCH_ASMT2_TOTAL_QUERY);
+      const [endSemGrades] = await db.query(FETCH_CONSOLIDATED_ES_QUERY);
 
     // Evaluating Continuous Evaluation CO
 
-    internalExam1 = consolidator(internalExam1, internalExam1Total[0]);
-    internalExam2 = consolidator(internalExam2, internalExam2Total[0]);
-    assignment1 = consolidator(assignment1, assignment1Total[0]);
-    assignment2 = consolidator(assignment2, assignment2Total[0]);
+      internalExam1 = consolidator(internalExam1, internalExam1Total[0]);
+      internalExam2 = consolidator(internalExam2, internalExam2Total[0]);
+      assignment1 = consolidator(assignment1, assignment1Total[0]);
+      assignment2 = consolidator(assignment2, assignment2Total[0]);
 
-    const continousEval = avgArray(avgArray(internalExam1, internalExam2, 0.5), avgArray(assignment1, assignment2, 0,5), 0.6);
+      const continousEval = avgArray(avgArray(internalExam1, internalExam2, 0.5), avgArray(assignment1, assignment2, 0,5), 0.6);
 
     // Evaluating End Sem CO
 
-    let passPercent = 0;
-    for(let i = 0 ; i < endSemGrades.length ; i++)
-    {
+      let passPercent = 0;
+      for(let i = 0 ; i < endSemGrades.length ; i++)
+      {
         if(calcGrade(endSemGrades[i].grade) >= 60)
             passPercent++; 
+      }
+
+      passParcent /= endSemGrades.length;
+
+      const endSemCO = coValGen(passPercent);
+      const endSemValues = [endSemCO, endSemCO, endSemCO, endSemCO, endSemCO, endSemCO];
+
+      // Evaluating Final CO Attainment
+
+      const finalValues = avgArray(endSemValues, continousEval, 0.6); // Final value array contains consolidated COs
+
+      } catch(err) {
+        return res.send({ success: false, message: err.message });
     }
-
-    passParcent /= endSemGrades.length;
-
-    const endSemCO = coValGen(passPercent);
-    const endSemValues = [endSemCO, endSemCO, endSemCO, endSemCO, endSemCO, endSemCO];
-
-    // Evaluating Final CO Attainment
-
-    const finalValues = avgArray(endSemValues, continousEval, 0.6); // Final value array contains consolidated COs
-
 
 }
